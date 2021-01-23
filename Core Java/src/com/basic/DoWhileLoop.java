17ecdf17aac8e0d5adebfce5cc99bbd17338c323
Suraj Gawai <66380264+surajgawai123@users.noreply.github.com>
2021-01-23 20:52:16 +0530

    SG message : adding syso in class as per US : BYN-111
    Signed-off-by: Suraj Gawai <66380264+surajgawai123@users.noreply.github.com>

--- Core Java/src/com/basic/DoWhileLoop.java
+++ Core Java/src/com/basic/DoWhileLoop.java
@@ -2,6 +2,7 @@ package com.basic;
 
 public class DoWhileLoop {
 	public static void main(String[] args) {
+		System.out.println("I am in main method");
 		int i = 0;
 		do {
 			System.out.println("i is :" + i);
